0d2e98a7a7f5f9c6662bcc80942cfad229223e4e
marlonfda <marlonfda@gmail.com>
2016-03-13 17:47:47 -0300

    Add tests

--- src/main/java/com/marlon/mockup/client/Account.java
+++ src/main/java/com/marlon/mockup/client/Account.java
@@ -7,6 +7,12 @@ package com.marlon.mockup.client;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.BindException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -17,22 +23,32 @@ import javax.ws.rs.core.Response;
  */
 public class Account {
     
+    public static final String BASE_URL = "http://localhost:8080/mockup/api/";
+    public static final String RESOURCE_PACKAGE = "com.marlon.mockup.api";
+    
     public static void main(String[] args){
         try{
+            createHttpServer();
             Client client = Client.create();
-            WebResource webResource = client.resource("http://localhost:8080/mockup/api/v1/user");
-            
+            WebResource webResource = client.resource(BASE_URL + "v1/user/");
             ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-            
             if(response.getStatus() != Response.Status.OK.getStatusCode()){
-                throw new Exception("Erro ao obter usuário: " + response.getStatus());
+                throw new Exception("Erro ao obter dados do usuário: " + response.getStatus());
             }
             String json = response.getEntity(String.class);
             System.out.println(json);
-            
         }catch(Exception e){
             e.printStackTrace();
         }
     }
     
+    private static void createHttpServer() {
+        try{
+            ResourceConfig resourceConfig = new PackagesResourceConfig(RESOURCE_PACKAGE);
+            HttpServer httpServer = HttpServerFactory.create(BASE_URL, resourceConfig);
+            httpServer.start();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/test/java/com/marlon/mockup/api/test/AccountServiceTest.java
@@ -0,0 +1,104 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.marlon.mockup.api.test;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.net.httpserver.HttpServer;
+import java.util.Date;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author marlon
+ */
+public class AccountServiceTest{
+    
+    public static final String BASE_URL = "http://localhost:8080/mockup/api/";
+    public static final String RESOURCE_PACKAGE = "com.marlon.mockup.api";
+    public static final String INFO_USER = "v1/user";
+    public static final long TIMEOUT = 200;
+    public HttpServer httpServer;
+    public Thread thread; 
+    public static long startTime;
+    public static long endTime;
+    
+    private static HttpServer createHttpServer() {
+        try{
+            ResourceConfig resourceConfig = new PackagesResourceConfig(RESOURCE_PACKAGE);
+            return HttpServerFactory.create(BASE_URL, resourceConfig);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    private void threadStart() throws Exception{
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startTime = new Date().getTime();
+                Client client = Client.create();
+                WebResource webResource = client.resource(BASE_URL + INFO_USER);
+                ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+                endTime = new Date().getTime();    
+            }
+        });
+        thread.start();
+    }
+    
+    @Before
+    public void setUp() throws Exception{
+        httpServer = createHttpServer();
+        if(httpServer != null && httpServer instanceof HttpServer){
+            httpServer.start();
+        }
+        if(thread == null){
+            threadStart();
+        }
+    }
+    
+    @After
+    public void tearDown() throws Exception{
+        if(httpServer != null && httpServer instanceof HttpServer){
+            httpServer.stop(0);
+        }
+        if(thread != null && thread instanceof Thread && thread.isAlive()){
+            thread.interrupt();
+        }
+    }
+    
+    /**
+     * Test of get method, of class AccountService.
+     */
+    @Test
+    public void testGet() throws Exception{
+        Client client = Client.create();
+        WebResource webResource = client.resource(BASE_URL + INFO_USER);
+        ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+        Assert.assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
+        String json = response.getEntity(String.class);
+        System.out.println(json);
+    }
+    
+    @Test(timeout = TIMEOUT)
+    public void timeoutTest() throws Exception {
+        if(thread != null && thread instanceof Thread){ // && thread.isAlive()  
+            Assert.assertTrue(endTime - startTime <= TIMEOUT);
+        }else{
+            throw new InterruptedException("Timeout");
+        }
+    }
+}
